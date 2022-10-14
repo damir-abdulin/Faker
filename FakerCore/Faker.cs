844ec53ff04dfc1a740cd50627e8cb127d42d95b
Damir Abdulin <damir.abdulin@tutanota.com>
2022-10-14 10:00:03 +0300

    [fix] Init core

---
+++ FakerCore/Faker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FakerCore
+{
+    public class Faker
+    {
+        public T Create<T>()
+        {
+            return (T) GetDefaultValue(typeof(T));
+        }
+
+        private object Create(Type t)
+        {
+            return null;
+        }
+
+        private object GetDefaultValue(Type t)
+        {
+            object defaultValue = null;
+            
+            if (t.IsValueType)
+                defaultValue = Activator.CreateInstance(t);
+            
+            return defaultValue;
+        }
+    }
+}
