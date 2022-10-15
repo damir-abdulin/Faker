b6f998d7ff0b523c20d5e196d7f1bb4898445dc8
Damir Abdulin <damir.abdulin@tutanota.com>
2022-10-15 20:16:54 +0300

    [feature] Add string generator

--- FakerCore/Generators/StringGenerator.cs
+++ FakerCore/Generators/StringGenerator.cs
@@ -5,14 +5,20 @@ public class StringGenerator : IGenerator
     private const int MinLength = 1;
     private const int MaxLength = 20;
     
+    private const string EnableChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
     public object Generate(Type type, GeneratorContext context)
     {
         var length = context.Random.Next(MinLength, MaxLength + 1);
 
         Span<char> buffer = stackalloc char[length];
 
+        for (var i = 0; i < length; i++)
+        {
+            buffer[i] = EnableChars[context.Random.Next(0, EnableChars.Length)];
+        }
         
-        return (uint)context.Random.Next();
+        return buffer.ToString();
     }
 
     public bool CanGenerate(Type type)
