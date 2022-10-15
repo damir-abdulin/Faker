d008ea5ca99f63b65ddcaff2a008be7733229b7c
Damir Abdulin <damir.abdulin@tutanota.com>
2022-10-15 20:04:06 +0300

    [feature] Add idea info

---
+++ FakerCore/Faker.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace FakerCore
+{
+    public class Faker
+    {
+        private IEnumerable<IGenerator> _generators;
+        private GeneratorContext _context;
+
+        public Faker()
+        {
+            GetGenerators();
+            _context = new GeneratorContext(this, new Random());
+        }
+        
+        public T Create<T>()
+        {
+            return (T) Create(typeof(T));
+        }
+
+        private object Create(Type t)
+        {
+            var newObject = _generators.Where(g => g.CanGenerate(t)).
+                Select(g => g.Generate(t, _context)).FirstOrDefault();
+
+            return newObject ?? GetDefaultValue(t);
+        }
+
+        private void GetGenerators()
+        {
+            _generators = Assembly.GetExecutingAssembly().GetTypes()
+                .Where(t => t.GetInterfaces().Contains(typeof(IGenerator)))
+                .Select(t => (IGenerator)Activator.CreateInstance(t));
+        }
+        
+        private static object GetDefaultValue(Type t)
+        {
+            return t.IsValueType ? Activator.CreateInstance(t) : null;
+        }
+    }
+}
