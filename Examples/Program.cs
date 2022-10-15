58edee17c9d0301a39250b2dee7572ab1f634f6e
Damir Abdulin <damir.abdulin@tutanota.com>
2022-10-15 11:43:40 +0300

    [feature] Example for bool generate

--- Examples/Program.cs
+++ Examples/Program.cs
@@ -1,9 +1,28 @@
-﻿namespace Examples
+﻿using System;
+using FakerCore;
+
+namespace Examples
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            var faker = new Faker();
+
+            int trueCount = 0;
+            int falseCount = 0;
+            
+            for (var i = 0; i < 1000; i++)
+            {
+
+                if (faker.Create<bool>())
+                    trueCount += 1;
+                else
+                    falseCount += 1;
+            }
+
+            Console.Write($"TRUE:  {trueCount}\nFALSE: {falseCount}");
+            
         }
     }
 }
