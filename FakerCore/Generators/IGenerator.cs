210dc7c133598d0ece1999202bfc969b5604b706
Damir Abdulin <damir.abdulin@tutanota.com>
2022-10-15 11:44:01 +0300

    [feature] Add interface for generators

---
+++ FakerCore/Generators/IGenerator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace FakerCore.Generators
+{
+    public interface IGenerator
+    {
+        object Generate(Type type, GeneratorContext context);
+        bool CanGenerate(Type type);
+    }
+}
