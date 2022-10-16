bf3661fff62f0ee374facaf0efba37d20345b01d
Damir Abdulin <damir.abdulin@tutanota.com>
2022-10-16 11:34:48 +0300

    [feature] Add generator for user classes

--- FakerCore/Faker.cs
+++ FakerCore/Faker.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
+﻿using System.Reflection;
 
 namespace FakerCore
 {
@@ -26,6 +23,12 @@ namespace FakerCore
             var newObject = _generators.Where(g => g.CanGenerate(t)).
                 Select(g => g.Generate(t, _context)).FirstOrDefault();
 
+            if (newObject is null)
+            {
+                // Generate object.
+                newObject = FillClass(t);
+            }
+            
             return newObject ?? GetDefaultValue(t);
         }
 
@@ -36,9 +39,53 @@ namespace FakerCore
                 .Select(t => (IGenerator)Activator.CreateInstance(t));
         }
         
-        private static object GetDefaultValue(Type t)
+        private object GetDefaultValue(Type t)
         {
             return t.IsValueType ? Activator.CreateInstance(t) : null;
+        }
+        
+        private object FillClass(Type t)
+        {
+            var newObject = CreateClass(t);
+            FillProperties(newObject, t);
+            FillFields(newObject, t);
+
+            return newObject;
+        }
+
+        private object CreateClass(Type t)
+        {
+            var constructorsInfo = t.GetConstructors(BindingFlags.DeclaredOnly | 
+                                                     BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+            
+            var paramsInfo = constructorsInfo[0].GetParameters();
+            var args = new object[paramsInfo.Length];
+
+            for (var i = 0; i < args.Length; i++)
+            {
+                args[i] = Create(paramsInfo[i].ParameterType);
+            }
+
+            return constructorsInfo[0].Invoke(args);
+        }
+
+        private void FillProperties(object obj, IReflect t)
+        {
+            var propertiesInfo = t.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            foreach (var property in propertiesInfo)
+            {
+                property.SetValue(obj, Create(property.PropertyType)); 
+            }
+        }
+
+        private void FillFields(object obj, IReflect t)
+        {
+            var fieldsInfo = t.GetFields(BindingFlags.Public | BindingFlags.Instance);
+            
+            foreach (var field in fieldsInfo)
+            {
+                field.SetValue(obj, Create(field.FieldType));
+            }
         }
     }
 }
