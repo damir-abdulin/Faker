40b8e8363aebdb6ce1221bb1f9ee621dc5d961ee
Damir Abdulin <damir.abdulin@tutanota.com>
2022-10-19 11:15:36 +0300

    [feature] Add methods for FakerConfig

--- FakerCore/FakerConfig.cs
+++ FakerCore/FakerConfig.cs
@@ -4,21 +4,35 @@ namespace FakerCore;
 
 public class FakerConfig
 {
-    private Dictionary<Type, Dictionary<string, IGenerator>> _generators;
+    private readonly Dictionary<Type, Dictionary<string, IGenerator>> _generators;
 
+    public FakerConfig()
+    {
+        _generators = new Dictionary<Type, Dictionary<string, IGenerator>>();
+    }
+    
     public void Add<TTypeName, TFieldType, TGenerator>(Expression<Func<TTypeName, TFieldType>> getField)
     {
-        // Create or open key Type.
-        // Get field name.
-        // Create or open key field name.
-        // Save generator to dictionary.
+        if (!_generators.ContainsKey(typeof(TTypeName)))
+        {
+            _generators.Add(typeof(TTypeName), new Dictionary<string, IGenerator>());
+        }
+        
+        var member = getField.Body as MemberExpression ?? throw new ArgumentException("Invalid expression");
+        var fieldName = member.Member.Name;
+        var generator = (IGenerator)Activator.CreateInstance(typeof(TGenerator));
+        
+        _generators[typeof(TTypeName)].Add(fieldName, generator);
+    }
 
-        throw new NotImplementedException();
+    public bool HasGenerator(Type type, string fieldName)
+    {
+        return _generators.ContainsKey(type) && _generators[type].ContainsKey(fieldName);
     }
 
     public IGenerator GetGenerator(Type type, string fieldName)
     {
-        throw new NotImplementedException();
+        return HasGenerator(type, fieldName) ? _generators[type][fieldName] : null;
     }
     
 }
