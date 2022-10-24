addd5d632007d6e2d39dc79cc27ce5f7a58e8db9
Damir Abdulin <damir.abdulin@tutanota.com>
2022-10-24 11:56:06 +0300

    [feature] Create examples

---
+++ Examples/Classes.cs
@@ -0,0 +1,31 @@
+﻿namespace Examples;
+
+public class A
+{
+    public string Ctor { get; }
+    public B B { get; set; }
+
+    public int IntField { get; set; }
+    public int FieldWithoutSet { get; }
+    public int FieldWithPrivateSet { get; private set; }
+    
+    public int IntValue;
+    public decimal DecimalValue; 
+    public short ShortValue;
+    public string StringValue;
+
+    private A()
+    {
+        Ctor = "A()";
+    }
+}
+
+public class B
+{
+    public C C { get; set; }
+}
+
+public class C
+{
+    public A A { get; set; }
+}
---
+++ Examples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+using Examples;
+using FakerCore;
+
+var faker = new Faker();
+
+var a = faker.Create<A>();
+
+Console.WriteLine($".ctor {a.Ctor};\nIntField: {a.IntField};\nFieldWithoutSet: {a.FieldWithoutSet};\n" +
+                  $"FieldWithPrivateSet: {a.FieldWithPrivateSet};\nIntValue: {a.IntValue};\n" +
+                  $"DecimalValue: {a.DecimalValue};\n ShortValue: {a.ShortValue};\n StringValue: {a.StringValue};");
+
