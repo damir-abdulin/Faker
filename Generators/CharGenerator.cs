bc0d6f8168d8fc53ddbaa646d6894ce65a585d06
Damir Abdulin <damir.abdulin@tutanota.com>
2022-10-24 11:55:49 +0300

    [feature] Create generators for dll

---
+++ Generators/CharGenerator.cs
@@ -0,0 +1,15 @@
+﻿namespace Generators;
+
+public class CharGenerator
+{
+    private static object Generate(Type type)
+    {
+        var random = new Random();
+        return (char)random.Next();
+    }
+
+    private static bool CanGenerate(Type type)
+    {
+        return type == typeof(char);
+    }
+}
